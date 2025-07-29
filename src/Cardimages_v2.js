be4222c7d029c5bc540c0cc3923a66ea4970ca7c
MT38Fire <andrea.schianchi04@gmail.com>
2025-07-29 17:41:17 +0200

    Add files via upload

---
+++ src/Cardimages_v2.js
@@ -0,0 +1,55 @@
+import img_0_0 from './assets/cards/c_0_0.png';
+import img_0_1 from './assets/cards/c_0_1.png';
+import img_0_2 from './assets/cards/c_0_2.png';
+import img_0_3 from './assets/cards/c_0_3.png';
+import img_0_4 from './assets/cards/c_0_4.png';
+import img_0_5 from './assets/cards/c_0_5.png';
+import img_0_6 from './assets/cards/c_0_6.png';
+import img_0_7 from './assets/cards/c_0_7.png';
+import img_0_8 from './assets/cards/c_0_8.png';
+import img_0_9 from './assets/cards/c_0_9.png';
+import img_1_0 from './assets/cards/c_3_0.png';
+import img_1_1 from './assets/cards/c_3_1.png';
+import img_1_2 from './assets/cards/c_3_2.png';
+import img_1_3 from './assets/cards/c_3_3.png';
+import img_1_4 from './assets/cards/c_3_4.png';
+import img_1_5 from './assets/cards/c_3_5.png';
+import img_1_6 from './assets/cards/c_3_6.png';
+import img_1_7 from './assets/cards/c_3_7.png';
+import img_1_8 from './assets/cards/c_3_8.png';
+import img_1_9 from './assets/cards/c_3_9.png';
+import img_2_0 from './assets/cards/c_1_0.png';
+import img_2_1 from './assets/cards/c_1_1.png';
+import img_2_2 from './assets/cards/c_1_2.png';
+import img_2_3 from './assets/cards/c_1_3.png';
+import img_2_4 from './assets/cards/c_1_4.png';
+import img_2_5 from './assets/cards/c_1_5.png';
+import img_2_6 from './assets/cards/c_1_6.png';
+import img_2_7 from './assets/cards/c_1_7.png';
+import img_2_8 from './assets/cards/c_1_8.png';
+import img_2_9 from './assets/cards/c_1_9.png';
+import img_3_0 from './assets/cards/c_2_0.png';
+import img_3_1 from './assets/cards/c_2_1.png';
+import img_3_2 from './assets/cards/c_2_2.png';
+import img_3_3 from './assets/cards/c_2_3.png';
+import img_3_4 from './assets/cards/c_2_4.png';
+import img_3_5 from './assets/cards/c_2_5.png';
+import img_3_6 from './assets/cards/c_2_6.png';
+import img_3_7 from './assets/cards/c_2_7.png';
+import img_3_8 from './assets/cards/c_2_8.png';
+import img_3_9 from './assets/cards/c_2_9.png';
+import back from './assets/cards/back.png';
+
+const IMAGES = {
+    img_0_0, img_0_1, img_0_2, img_0_3, img_0_4,
+    img_0_5, img_0_6, img_0_7, img_0_8, img_0_9,   //Bastoni
+    img_1_0, img_1_1, img_1_2, img_1_3, img_1_4,
+    img_1_5, img_1_6, img_1_7, img_1_8, img_1_9,   //Spade
+    img_2_0, img_2_1, img_2_2, img_2_3, img_2_4,
+    img_2_5, img_2_6, img_2_7, img_2_8, img_2_9,   //Coppe
+    img_3_0, img_3_1, img_3_2, img_3_3, img_3_4,
+    img_3_5, img_3_6, img_3_7, img_3_8, img_3_9,   //Denari
+    back
+};
+
+export default IMAGES;
